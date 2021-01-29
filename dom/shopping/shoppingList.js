717831b720600f15f164dba28a99db3205424cd3
Jordan Slim <sjordanslim@gmail.com>
2021-01-29 10:21:27 -0600

    finished shoppinglist

--- dom/shopping/shoppingList.js
+++ dom/shopping/shoppingList.js
@@ -37,30 +37,3 @@
           //  10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
           input.focus();
     }
-
-/*
-    const list = document.querySelector('ul');
-    const input = document.querySelector('input');
-    const button = document.querySelector('button');
-
-    button.onclick = function() {
-      let myItem = input.value;
-      input.value = '';
-
-      const listItem = document.createElement('li');
-      const listText = document.createElement('span');
-      const listBtn = document.createElement('button');
-
-      listItem.appendChild(listText);
-      listText.textContent = myItem;
-      listItem.appendChild(listBtn);
-      listBtn.textContent = 'Delete';
-      list.appendChild(listItem);
-
-      listBtn.onclick = function(e) {
-        list.removeChild(listItem);
-      }
-
-      input.focus();
-    }
-*/
