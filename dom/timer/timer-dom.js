da237079e2a26b7c2ff866cfec53d6be2b70fa2f
Jordan Slim <sjordanslim@gmail.com>
2021-01-29 10:20:37 -0600

    finished shoppinglist

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,13 @@
 console.log("time to start the timer!");
 
+let sec = 0;
+let min = 0;
+let onOff = 0;
+
+document.getElementById("start").addEventListener("click", startTime());
+document.getElementById("stop").addEventListener("click", stopTime());
+document.getElementById("restart").addEventListener("click", restartTime());
+
+function startTime(){}
+function stopTime(){}
+function restartTime(){}
