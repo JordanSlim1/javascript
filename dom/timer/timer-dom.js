fb5b88676344684a0ad8f78978be4536519bbbca
Jordan Slim <sjordanslim@gmail.com>
2021-01-31 15:06:24 -0600

    finished the timer

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -3,46 +3,45 @@ console.log("time to start the timer!");
 let sec = 0;
 let min = 0;
 let onOff = 0;
-var timer;
+let interval = null;
+function count(){
+sec++;
+if(sec < 60){
+    document.getElementById("timer").innerHTML = min + ":" + sec;
+    if(sec < 10){
+    document.getElementById("timer").innerHTML = min + ":" + "0" + sec;
+    }
+}
+if(sec/60 == 1){
+    sec = 0;
+    min++;
+    if(sec < 10){
+        document.getElementById("timer").innerHTML = min + ":" + "0" + sec;
+    }
+    document.getElementById("timer").innerHTML = min + ":" + sec;
+}
+}
 
-let startTime = document.getElementById('start');
-let stopTime = document.getElementById('stop');
-let restartTime = document.getElementById('restart');
-let mid = document.getElementById('timer');
-
-let para = document.querySelector('p');
-
-mid.appendChild(para);
-mid.appendChild(startTime);
-mid.appendChild(stopTime);
-mid.appendChild(restartTime);
-
-startTime.onclick = function(sec, min, onOff){
+function startTime(){
     onOff = 1;
-    sec = parseInt(sec);
-    min = parseInt(min);
-    timer = setInterval(startTime, 1000);
-
     if(onOff == 1){
-        if(sec < 59){
-             sec = sec + 1;
-        }
-        if(sec == 60){
-             min = min + 1;
-            sec = 0;
-         }
-        para.textContent = min + ":" + sec;
+        interval = window.setInterval(count , 100);
+        document.getElementById("start").innerHTML = "Start";
     }
 }
 
-stopTime.onclick = function(sec, min, onOff){
+function stopTimer(){
     onOff = 0;
-    para.textContent = min + ":" + sec;
+    if(onOff == 0){
+        window.clearInterval(interval);
+        document.getElementById("stop").innerHTML = "Stop";
+    }
 }
 
-restartTime.onclick = function(onOff, min, sec){
+function restartTimer(){
+    window.clearInterval(interval);
     onOff = 0;
     min = 0;
     sec = 0;
-    para.textContent = min + ":" + sec;
+    document.getElementById("timer").innerHTML = "0:00";
 }
